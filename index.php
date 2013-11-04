4b7ca881ba219594eda313a7eee0be9cbc756e09
Myke Bates <mbates@thealchemediaproject.com>
2013-11-04 00:07:50 -0600

    first round of work

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php'); ?>
+
+    <div class="container animated slideInDown">
+        <img src="images/horse.jpg" alt="">
+        <h1>NEIGH</h1>
+    </div>
+
+<?php include('includes/footer.php'); ?>
