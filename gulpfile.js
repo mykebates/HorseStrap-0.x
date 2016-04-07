893e8124619d80574b4c14034d6ce7a156b052c9
Sean Jones <sean.daniel.jones1985@gmail.com>
2016-04-07 12:58:22 -0500

    jigsaw take 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+var _if = require('gulp-if');
 
 elixir.config.assetsPath = 'source/_assets';
 elixir.config.publicPath = 'source';
 
 elixir(function(mix) {
+    var isWindows = /^win/.test(require('os').platform());
+    var jigsaw =_if(!isWindows, './vendor/bin/jigsaw build', 'vendor\\bin\\jigsaw build');
     mix.sass('main.scss')
-        .exec('./vendor/bin/jigsaw build', ['./source/**/*', '!./source/_assets/**/*'])
+        .exec(jigsaw, ['./source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             server: { baseDir: 'build_local' },
             proxy: null,
