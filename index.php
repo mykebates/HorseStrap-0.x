cfc8b6ef9e833a14a5768edd88a9fd07dff1c039
Sean Jones <sean.daniel.jones1985@gmail.com>
2016-04-07 10:20:39 -0500

    removing old structures

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include('includes/header.php'); ?>
-
-    <div class="container animated slideInDown">
-        <img src="images/horse.jpg" alt="">
-        <h1>NEIGH</h1>
-    </div>
-
-<?php include('includes/footer.php'); ?>
