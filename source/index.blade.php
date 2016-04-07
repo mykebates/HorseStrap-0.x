f730993a410df8fcc0eda7a5bc933ec80c2dd932
Sean Jones <sean.daniel.jones1985@gmail.com>
2016-04-07 10:22:02 -0500

    adding new structures/jigsaw

---
+++ source/index.blade.php
@@ -0,0 +1,8 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="container animated slideInDown">
+        <img src="images/horse.jpg" alt="">
+        <h1>NEIGHhhh</h1>
+    </div>
+@endsection
